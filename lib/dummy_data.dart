d049320fe7c3391fe3d8029e730ea80440ffebc6
Maulana Wes <maulanawest@arwics.com>
2021-07-27 01:31:32 +0700

    Adding base data

---
+++ lib/dummy_data.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import './model/category_mode.dart';
+
+const DUMMY_CATEGORIES = const [
+  CategoryModel(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  CategoryModel(
+    id: 'c2',
+    title: 'Indonesia',
+    color: Colors.orange,
+  ),
+  CategoryModel(
+    id: 'c3',
+    title: 'Japan',
+    color: Colors.grey,
+  ),
+  CategoryModel(
+    id: 'c4',
+    title: 'England',
+    color: Colors.green,
+  ),
+  CategoryModel(
+    id: 'c5',
+    title: 'Belanda',
+    color: Colors.blue,
+  ),
+  CategoryModel(
+    id: 'c6',
+    title: 'Swiss',
+    color: Colors.yellowAccent,
+  ),
+  CategoryModel(
+    id: 'c7',
+    title: 'Spain',
+    color: Colors.brown,
+  ),
+];
